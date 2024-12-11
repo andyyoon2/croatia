07239c26ec243218850214bcf42fa9771a9cf9af
Andy Yoon <insoo.yoon@proton.me>
2024-12-11 08:46:05 -0800

    Display countdown!

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Task exposing (Task)
 import Time exposing (Month(..))
@@ -79,6 +80,29 @@ monthToString month =
     Nov -> "November"
     Dec -> "December"
 
+-- Dates - I just converted these from JS Dates
+departDate = 1737187200000
+returnDate = 1743408000000
+
+dateDeltaDisplay : Int -> Int -> String
+dateDeltaDisplay start end =
+  let
+    delta = end - start
+  in
+    if delta <= 0 then
+      "Start is before end. Not Implemented!"
+    else
+      let
+        totalSeconds = delta // 1000
+        totalMinutes = totalSeconds // 60
+        totalHours = totalMinutes // 60
+        totalDays = totalHours // 24
+      in
+        String.fromInt totalDays ++ " days, " ++
+        String.fromInt (modBy 24 totalHours) ++ " hours, " ++
+        String.fromInt (modBy 60 totalMinutes) ++ " minutes"
+
+
 -- VIEW
 
 view : Model -> Html Msg
@@ -88,14 +112,15 @@ view model =
     month = monthToString (Time.toMonth model.zone model.time)
     year = String.fromInt (Time.toYear model.zone model.time)
   in
-  main_ []
+  main_ [ style "padding" "16px", style "textAlign" "center" ]
     [ heading 
     , time day month year
+    , p [] [ text (dateDeltaDisplay (Time.posixToMillis model.time) departDate ++ " until takeoff!") ]
     ]
 
 heading =
   div []
-    [ h1 [] [ text "Hello Elm!" ]
+    [ h1 [] [ text "Croatia Countdown" ]
     ]
 
 time : String -> String -> String -> Html Msg
